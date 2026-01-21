8424ddd14a967dd92881cdaa128a7a5026cded63
9calle <carth965@student.liu.se>
2026-01-21 14:55:12 +0100

    Cast bar ticker

--- UnitFrames.lua
+++ UnitFrames.lua
@@ -689,8 +689,10 @@ function SetupCastBar(unitFrame)
             or event == "UNIT_SPELLCAST_CHANNEL_STOP"
             or event == "UNIT_SPELLCAST_INTERRUPTED"
             or event == "UNIT_SPELLCAST_EMPOWER_STOP" then
+            self.Ticker:Cancel()
             self:Hide()
         elseif event == "PLAYER_FOCUS_CHANGED" or event == "PLAYER_TARGET_CHANGED" then
+            if self.Ticker then self.Ticker:Cancel() end
             UpdateCastBar(self, nil, nil)
         end
     end)
